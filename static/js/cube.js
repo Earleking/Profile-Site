0a0af601a03ef05c661b561b66179a7416989b72
Earleking <arek.fielding@gmail.com>
2018-07-30 11:02:55 -0400

    updated experience section

--- static/js/cube.js
+++ static/js/cube.js
@@ -1,29 +1,4 @@
-var sideListInNormal = ['front', 'left', 'back', 'right']
-function rotateLeft() {
-    var cube = document.getElementById('cube');
-    var direction = '';
-    var lastclass = cube.classList[cube.classList.length - 1];
-    var newClass = 'cube-show-';
-    console.log(cube.style.transform.length)
-    try {
-        direction = lastclass.split('-')[2];
-    } catch (error) {
-        
-    }
-    for(i in sideListInNormal){
-        if(sideListInNormal[i] == direction){
-            cube.classList.remove(lastclass);
-            if(i == 0){
-                i = 4;
-            }
-            console.log((i - 1) % (sideListInNormal.length + 1));
-            newClass = newClass + sideListInNormal[(i - 1) % (sideListInNormal.length + 1)];
-            cube.classList.add(newClass);
-            console.log(newClass);
-            return;
-        }
-    }
-}
+var sideListInNormal = ['front', 'left', 'right']
 function rotateRight() {
     var cube = document.getElementById('cube');
     var direction = '';
@@ -38,14 +13,64 @@ function rotateRight() {
     for(i in sideListInNormal){
         if(sideListInNormal[i] == direction){
             cube.classList.remove(lastclass);
-            if(i == 3){
+            // remove z-index class from face
+            // This is needed because rendering works in a weird order sometimes
+            var face = document.getElementById('work-' + sideListInNormal[i] + "-face");
+            try {
+                face.classList.remove('to-front')
+            } catch (error) {
+                
+            }
+            if(i == 0){
+                i = sideListInNormal.length;
+            }
+            nextIndex = (i - 1) % (sideListInNormal.length + 1);
+            // readd z-index class to new face
+            face = document.getElementById('work-' + sideListInNormal[nextIndex] + "-face");
+            face.classList.add('to-front');
+
+            newClass = newClass + sideListInNormal[nextIndex];
+            cube.classList.add(newClass);
+            console.log(face);
+            return;
+        }
+    }
+}
+function rotateLeft() {
+    var cube = document.getElementById('cube');
+    var direction = '';
+    var lastclass = cube.classList[cube.classList.length - 1];
+    var newClass = 'cube-show-';
+    // console.log(cube.style.transform.length)
+    try {
+        direction = lastclass.split('-')[2];
+    } catch (error) {
+        
+    }
+    for(i in sideListInNormal){
+        if(sideListInNormal[i] == direction){
+            cube.classList.remove(lastclass);
+            // Remove z-index class from current face
+            
+            var face = document.getElementById('work-' + sideListInNormal[i] + "-face");
+            try {
+                face.classList.remove('to-front')
+            } catch (error) {
+                
+            }
+
+            if(i == sideListInNormal.length - 1){
                 i = -1;
             }
-            console.log(i);
-            console.log((i + 1));
-            newClass = newClass + sideListInNormal[(+i + +1) % (sideListInNormal.length + 1)];
+            nextIndex = (+i + +1) % (sideListInNormal.length + 1);
+
+            // Re-add z-index class
+            face = document.getElementById('work-' + sideListInNormal[nextIndex] + "-face");
+            face.classList.add('to-front');
+            console.log('work-' + sideListInNormal[nextIndex] + "-face");
+
+            newClass = newClass + sideListInNormal[nextIndex];
             cube.classList.add(newClass);
-            console.log(newClass);
             return;
         }
     }
