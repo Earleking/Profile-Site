eee3a9207e3729a948de52160b789826856a8d53
Earleking <arek.fielding@gmail.com>
2018-08-01 05:12:10 -0400

    updated for mobile

--- static/js/template.js
+++ static/js/template.js
@@ -2,7 +2,7 @@ var siteWidth = 1280;
 var scale = screen.width / siteWidth
 console.log(screen.width);
 console.log(scale);
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+// document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
 $('#load-overlay').css('visibility', 'hidden');
 $(function() {
     $(".nav-item > a").click(function(e) {
