a98ffae0768e186c47e615e9e20c442cae88017e
Earleking <arek.fielding@gmail.com>
2018-08-01 11:43:29 -0400

    added basic contact page

--- static/js/template.js
+++ static/js/template.js
@@ -5,12 +5,12 @@ console.log(scale);
 // document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
 $('#load-overlay').css('visibility', 'hidden');
 $(function() {
-    $(".nav-item > a").click(function(e) {
+    $(".nav-item > .suppress").click(function(e) {
         e.preventDefault(); //so the browser doesn't follow the link
         window.history.pushState("", 'test', this.href);
         window.scrollTo(0, 0);
         $('#load-overlay').css('visibility', 'visible');
-        $("#main").load(this.href, function() {
+        $("#main").load(this.href, {'test': 'hello'} , function() {
             //execute here after load completed
             $('#load-overlay').css('visibility', 'hidden');
         });
--- views/views.py
+++ views/views.py
@@ -1,17 +1,31 @@
-from flask import views, Blueprint, render_template, redirect, url_for
+from flask import views, Blueprint, render_template, redirect, url_for, request
 
 main = Blueprint('view', __name__, url_prefix='/')
 
-@main.route('')
+@main.route('', methods=['POST', 'GET'])
 def home():
     '''
     Main Page
     '''
-    return render_template('homePage.html', title="Hello World")
+    if request.method == 'POST':
+        return render_template('homePage.html', title="Arek Fielding")
+    return render_template('homePage.html', title="Arek Fielding", inital_page=True)
 
-@main.route('aboutMe')
+@main.route('aboutMe', methods=['POST', 'GET'])
 def about_me():
     '''
     About me
     '''
-    return render_template('aboutMe.html', title='About Me')
+    if request.method == 'POST':
+        return render_template('aboutMe.html', title='About Me')
+    return render_template('aboutMe.html', title='About Me', inital_page=True)
+
+@main.route('contactMe', methods=['POST', 'GET'])
+def contact():
+    '''
+    Contact Page
+    '''
+    if request.method == 'POST':
+        return render_template('contactMe.html', title='Contact Me')
+    return render_template('contactMe.html', title='Contact Me', inital_page=True)
+
