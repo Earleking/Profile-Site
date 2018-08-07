6e66bdfeda4995a6def498dd88a797e69b868d80
Earleking <arek.fielding@gmail.com>
2018-08-07 15:58:08 -0400

    sliding drawer

--- static/js/template.js
+++ static/js/template.js
@@ -1,7 +1,7 @@
-var siteWidth = 1280;
-var scale = screen.width / siteWidth
-console.log(screen.width);
-console.log(scale);
+// var siteWidth = 1280;
+// var scale = screen.width / siteWidth
+// console.log(screen.width);
+// console.log(scale);
 // document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
 // $('#load-overlay').css('visibility', 'hidden');
 // $(function() {
@@ -16,3 +16,18 @@ console.log(scale);
 //         });
 //     });
 // });
+var navbarShown = false;
+function toggleNavbar() {
+    var navbar = document.getElementById('navbar');
+    var icon = document.getElementById('hamburger-button');
+    if(navbarShown) {
+        navbar.style.left = "-70px";
+        icon.style.left = "0";
+        navbarShown = false;
+    }
+    else {
+        navbar.style.left = "0";
+        icon.style.left = "70px";
+        navbarShown = true;
+    }
+}
