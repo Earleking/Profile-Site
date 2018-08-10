fe0937276fa0f1ac5279748611bbe5dad037de99
Earleking <arek.fielding@gmail.com>
2018-08-10 11:07:31 -0400

    overlay fades properly

--- static/js/projects.js
+++ static/js/projects.js
@@ -1,20 +1,13 @@
 var curProject = 0;
 function fadeIn(newProj) {
-    var project;
+    var project  = document.getElementById('project-' + newProj);
     if (curProject != 0) {
-        // set it to 0
-        project = document.getElementById('project-' + curProject);
-        project.classList.remove('show-overlay');
-        project.classList.add('hide-overlay');
-        curProject = 0;
-        // return;
+        fadeOut();
     }
     try {
         project.classList.remove('hide-overlay');
     } catch (error) {
-        
     }
-    project = document.getElementById('project-' + newProj);
     project.classList.add("show-overlay");
     curProject = newProj;
 }
@@ -23,5 +16,5 @@ function fadeOut() {
     project = document.getElementById('project-' + curProject);
     project.classList.remove('show-overlay');
     project.classList.add("hide-overlay");
-    curProject = newProj;
+    curProject = 0;
 } 
